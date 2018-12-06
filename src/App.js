9efb238317b688390e700f35560d349b7dac0054
Daniel <hoptond848@protonmail.com>
2018-12-06 16:56:32 +0000

    bind this.getTasks in src/App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,31 @@ import List from './List'
 import './Update.js'
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            tasks: []
+        }
+        this.getTasks = this.getTasks.bind(this)
+        this.getTasks()
+    }
+
     render() {
+        let tasks = this.state.tasks
         return (
             <div>
                 <h1>Blue Todo</h1>
-                <List tasks={[{id: 1, desc: 'feed grass', status: false}, {id: 7, desc: 'mow cat', status: true}]}/>
+                <List tasks={tasks}/>
             </div>
         )
+    }
+
+    async getTasks() {
+        let response = await fetch('http://localhost:8080/todos/', {
+            method: 'get'
+        })
+        let tasks = await response.json()
+        this.setState({tasks: tasks})
     }
 }
 
