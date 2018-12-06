5c5fd17bb89159a543d22a0d46e9da0f11a4f080
Daniel <hoptond848@protonmail.com>
2018-12-06 16:08:06 +0000

    send new task requests to server

--- src/NewTask.js
+++ src/NewTask.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
 class NewTask extends Component {
+
+    submitData(e) {
+            e.preventDefault()
+            console.log('sent fetch data')
+            fetch('http://localhost:8080/todos/', {
+                method: 'post',
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({desc: document.getElementById('newtask').value})
+            }).then(res => res.json())
+                .then(res => console.log(res));
+        }
+
+
     render() {
         return (
-            <form method="post" action="todos/new">
+            <form onSubmit={this.submitData}>
                 <li>
-                    <input name="desc" type="text" placeholder="Enter a new task here..."/>
+                    <input name="desc" id="newtask" ref={(input) => this.textInput = input} type="text" placeholder="Enter a new task here..."/>
                         <input type="submit" value="SUBMIT"/>
                 </li>
             </form>
