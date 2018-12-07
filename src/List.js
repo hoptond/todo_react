26a053565a95444a168ebf7ee9314178745ed710
Daniel <hoptond848@protonmail.com>
2018-12-07 09:45:01 +0000

    remove constructor from List and pass tasks as prop

--- src/List.js
+++ src/List.js
@@ -3,20 +3,13 @@ import NewTask from './NewTask'
 import Task from "./Task";
 
 class List extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            //tasks is an array of objects, each with an id and desc properties
-            tasks: props.tasks
-        };
-    }
 
     render() {
         return (
             <div className="List">
                 <ul>
                     {
-                        this.state.tasks.map((task) => {
+                        this.props.tasks.map((task) => {
                             return <Task id={task.id} desc={task.desc} status={task.status}/>
                         })
                     }
@@ -25,6 +18,7 @@ class List extends Component {
             </div>
         );
     }
+
 }
 
 export default List
